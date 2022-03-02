b0d0a6962d6acd7ed286ac9f740b424de2179a29
Paolo Rechia <paolorechia@gmail.com>
2022-03-02 12:40:45 +0100

    Start collection

--- src/collections.py
+++ src/collections.py
@@ -1,3 +1,16 @@
+from .items import Item
+from typing import Optional
+
+
 class Collection:
     def __init__(self):
-        pass
+        self.items = []
+
+    def add(self, item: Item):
+        self.items.append(item)
+
+    def next(self) -> Optional[Item]:
+        return None
+
+    def __len__(self) -> int:
+        return len(self.items)
--- test/test_collections.py
+++ test/test_collections.py
@@ -1,6 +1,23 @@
-from src import Collection
+import src as sidebrain
 
 
-def test_collection_is_instantiable():
-    c = Collection()
-    assert c
+def test_collection_starts_empty():
+    c = sidebrain.Collection()
+    assert len(c) == 0
+
+
+def test_collection_add_item():
+    c = sidebrain.Collection()
+
+    item = sidebrain.Item()
+    item.set_text_type("a")
+    item.set_answer("b")
+
+    c.add(item)
+    assert len(c) == 1
+
+
+def test_empty_collection_next_returns_none():
+    c = sidebrain.Collection()
+    item = c.next()
+    assert not item
